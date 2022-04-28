33464577ea21d6bc4fe0a54ec07a5c7bdc0465ba
Md Nakibul Hosen Nahid Nahid <nakibulh.aobs@gmail.com>
2022-04-28 17:43:20 +0600

    add user post route

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -2,12 +2,23 @@
 const express = require("express");
 
 // internal imports
-const { getUsers } = require("../controller/usersController");
+const { getUsers, addUser } = require("../controller/usersController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const avatarUpload = require("../middlewares/users/avatarUpload");
+const {
+  addUserValidator,
+  addUserValidationHandler,
+} = require("../middlewares/users/userValidators");
 
 const router = express.Router();
 
 // login
 router.get("/", decorateHtmlResponse("User"), getUsers);
-
+router.post(
+  "/",
+  avatarUpload,
+  addUserValidator,
+  addUserValidationHandler,
+  addUser
+);
 module.exports = router;
