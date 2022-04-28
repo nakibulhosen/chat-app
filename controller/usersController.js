2ab1aa1e039c8a4b477e8f802996773d33218689
Md Nakibul Hosen Nahid Nahid <nakibulh.aobs@gmail.com>
2022-04-28 17:43:37 +0600

    user post route controller

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,5 +1,62 @@
-function getUsers(req, res, next) {
-  res.render("users");
+// external modules
+const bcrypt = require("bcrypt");
+
+// internal modules
+const User = require("../models/peoples");
+
+// get user page
+async function getUsers(req, res, next) {
+  try {
+    const users = await User.find();
+    res.render("users", {
+      users: users,
+    });
+  } catch (err) {
+    next(err);
+  }
 }
 
-module.exports = { getUsers };
+// add user
+async function addUser(req, res, next) {
+  let newUser;
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+  if (req.files && req.files.length > 0) {
+    console.log("There are files", req.files[0]);
+    console.log({
+      ...req.body,
+      avatar: req.files[0].filename,
+      password: hashedPassword,
+    });
+    newUser = new User({
+      ...req.body,
+      avatar: req.files[0].filename,
+      password: hashedPassword,
+    });
+  } else {
+    console.log("there are not files ", req.files);
+    newUser = new User({
+      ...req.body,
+      password: hashedPassword,
+    });
+  }
+
+  // save the user or send error
+  try {
+    const result = await newUser.save();
+    res.status(200).json({
+      message: "User was added successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      errors: {
+        common: {
+          message: "Unknown error occured",
+          error: error,
+        },
+      },
+    });
+  }
+}
+
+module.exports = { getUsers, addUser };
