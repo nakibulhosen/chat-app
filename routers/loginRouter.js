ac1462011f76344e386028a407b22ac2530aaba9
Md Nakibul Hosen Nahid Nahid <nakibulh.aobs@gmail.com>
2022-05-02 12:24:45 +0600

    add login router

--- routers/loginRouter.js
+++ routers/loginRouter.js
@@ -2,11 +2,24 @@
 const express = require("express");
 
 // internal imports
-const { getLogin } = require("../controller/loginController");
+const { getLogin, login } = require("../controller/loginController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const {
+  loginValidators,
+  loginValidatorHandler,
+} = require("../middlewares/login/loginValidator");
+
 const router = express.Router();
 
+const pageTitle = "Login";
 // login
-router.get("/", decorateHtmlResponse("Login"), getLogin);
+router.get("/", decorateHtmlResponse(pageTitle), getLogin);
+router.post(
+  "/",
+  decorateHtmlResponse(pageTitle),
+  loginValidators,
+  loginValidatorHandler,
+  login
+);
 
 module.exports = router;
